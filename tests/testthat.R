852d6902b104637af2c32bdda6c0e53120c46ff8
mattkcole <mattcole1093@gmail.com>
2017-04-17 23:22:37 -0400

    adding tests, updating readme, setting up vignette

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(FAtools)
+
+test_check("FAtools")
---
+++ tests/testthat/loadings_plot_test.R
@@ -0,0 +1,10 @@
+context("this is the test purpose")
+
+test_that("test_label", {
+        expect_sucess()
+})
+
+test_that("test_vectors", {
+        expect_error()
+})
+
